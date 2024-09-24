dcce3f7f20a254a4e1eb322ec8e7458822827b48
yeshi16 <yeshi16@gmail.com>
2024-09-24 14:08:18 -0400

    update

--- articleWithFloatingImage/app.js
+++ articleWithFloatingImage/app.js
@@ -1,27 +1,25 @@
 gsap.registerPlugin(ScrollTrigger) 
 
-// console.log(gsap.utils.toArray(".arrow"))
-
+// // console.log(gsap.utils.toArray(".arrow"))
 
 
 gsap.utils.toArray(".arrow").forEach((arrow) => {
-   
-    gsap.fromTo(arrow,
-        {
-         autoAlpha: 0.7,  // similar to opacity: 0,
-          filter: "grayscale(100%)",
-        },
-        {
-          duration: 3,
-          opacity: 1,
-          filter: "grayscale(0)",
-          ScrollTrigger: {
-            trigger: arrow,
-            start: "top center", // top of the image and center of the view point (window)
-            end: "botom 30%", // bottom of the image and 30% bellow the top of view point
-            marker: true,
-            toggleActions: "restart reverse none reverse" // onEnter, onLeave, onEnterBack, onLeaveBack
-          }
-        }
-      );
+  gsap.fromTo(arrow,
+    {
+      autoAlpha: 0.7, // similar to opacity: 0,
+      filter: "grayscale(100%)"
+    },
+    {
+      duration: 3,
+      autoAlpha: 1,
+      filter: "grayscale(0%)",
+      scrollTrigger: {
+        trigger: arrow,
+        start: "top center",  // top of the image and center of the view point (window)                
+        end: "bottom 30%",   // bottom of the image and 30% bellow the top of view point
+        markers: true,
+        toggleActions: "restart reverse none reverse", // onEnter, onLeave, onEnterBack, onLeaveBack
+      }
+    }
+  );
 });
